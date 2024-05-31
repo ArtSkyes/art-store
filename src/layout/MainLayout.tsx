458dd768452dcb942eb84724429761fc9b056e96
Art <honeydewart@gmail.com>
2024-05-31 23:10:06 +0800

    removed unused import

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Box, Container, Grid } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
